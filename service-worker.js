62641d0b7ab54b7fc158cdba59d8095ac702ad0a
hariom-2010 <hariomnarayan2010@gmail.com>
2026-08-03 23:25:32 +0530

    Add service worker for file caching
    Implement service worker for caching files

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,25 @@
+const cacheName = "hariom-calculator-v1";
 
+const filesToCache = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(cacheName)
+    .then(cache => cache.addAll(filesToCache))
+  );
+});
+
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+    .then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
