bffe1ba819bffbfba56abe7d06b2e5b0c7aacd48
hariom-2010 <hariomnarayan2010@gmail.com>
2026-08-03 23:51:29 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,44 @@
-const cacheName = "hariom-calculator-v1";
+const CACHE_NAME = "hariom-calculator-v1";
 
-const filesToCache = [
+const FILES_TO_CACHE = [
   "./",
   "./index.html",
-  "./style.css",
-  "./script.js"
+  "./styles.css",
+  "./script.js",
+  "./manifest.json",
+  "./icon.png"
 ];
 
-self.addEventListener("install", event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(cacheName)
-    .then(cache => cache.addAll(filesToCache))
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(FILES_TO_CACHE);
+      })
   );
 });
 
 
-self.addEventListener("fetch", event => {
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+
+self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request)
-    .then(response => {
-      return response || fetch(event.request);
-    })
+      .then((response) => {
+        return response || fetch(event.request);
+      })
   );
 });
